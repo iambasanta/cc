2f49966901cb44077d21803de8d9fa9b83c4b617
iambasanta <sangpangbasanta@gmail.com>
2021-11-11 19:00:08 +0545

    write bubbleSort program to sort an array

---
+++ 3rd-sem/dsa/practicals/bubble_sort.c
@@ -0,0 +1,34 @@
+// WAP to sort an array using bubble sort
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+
+    int array[n];
+    printf("Enter array elements:\n");
+    for(int i = 0; i<n; i++){
+        scanf("%d",&array[i]);
+    }
+
+    int iterator = 1;
+    while(iterator < n){
+        for(int i = 0; i<n-iterator; i++){
+          if(array[i] > array[i+1]){
+              int temp = array[i];
+              array[i] = array[i+1];
+              array[i+1] = temp;
+          }
+        }
+        iterator ++;
+    }
+
+    printf("Sorted array :\n");
+    for(int i = 0; i<n; i++){
+        printf("%d ",array[i]);
+    }
+
+    return 0;
+}
