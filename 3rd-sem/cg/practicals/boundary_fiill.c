b548ebad936371784a61563068b161a376ed90b2
iambasanta <sangpangbasanta@gmail.com>
2022-02-06 10:07:45 +0545

    WAP to illustrate boundary fill algorithm

---
+++ 3rd-sem/cg/practicals/boundary_fiill.c
@@ -0,0 +1,27 @@
+#include<stdio.h> 
+#include<graphics.h>
+#include<dos.h> 
+void boundaryfill(int x,int y,int fill_color,int boundary_color){ 
+    if(getpixel(x,y)!=boundary_color && getpixel(x,y)!=fill_color){ delay(20);
+
+        putpixel(x,y,fill_color);
+        boundaryfill(x+1,y,fill_color,boundary_color);
+        boundaryfill(x,y+1,fill_color,boundary_color);
+        boundaryfill(x-1,y,fill_color,boundary_color);
+        boundaryfill(x,y-1,fill_color,boundary_color);
+    }
+} 
+int main() {
+    int gd=DETECT,gm;
+    int x,y,r;
+    initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+    printf("Enter x and y positions for circle\n");
+    scanf("%d%d",&x,&y);
+    printf("Enter radius of circle\n");
+    scanf("%d",&r);
+    circle(x,y,r);
+    boundaryfill(x,y,4,15);
+    outtextxy(200,150, “Boundary Fill”) getch();
+    closegraph();
+    return 0;
+} 
