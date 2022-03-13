92d38c924992aab9e4b06d138a33275498ef6970
iambasanta <sangpangbasanta@gmail.com>
2022-03-13 22:03:20 +0545

    Uppdate fobonacci program

--- 3rd-sem/dsa/exam/fibonacci_series.c
+++ 3rd-sem/dsa/exam/fibonacci_series.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
+/* int fibonacci(int n){ */
+/*     if(n==1 || n==2){ */
+/*         return 1; */
+/*     }else{ */
+/*         return fibonacci(n-1) + fibonacci(n-2); */
+/*     } */
+/* } */
+
 int fibonacci(int n){
     if(n==1){
         return 0;
