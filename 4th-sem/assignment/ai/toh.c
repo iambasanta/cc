c1db5b68fbbf169187d2eebc4b837f301569b5ff
iambasanta <sangpangbasanta@gmail.com>
2022-05-03 21:27:42 +0545

    Add program and output for tower of hanio

--- 4th-sem/assignment/ai/toh.c
+++ 4th-sem/assignment/ai/toh.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
 
-void TOH(int n, char A, char B, char C){
+void TOH(int n, char src[], char aux[], char dst[]){
     if(n>0){
-        TOH(n-1,A,C,B);
-        printf("Move disk %d from %c to %c. \n",n,A,C);
-        TOH(n-1,B,A,C);
+        TOH(n-1,src,dst,aux);
+        printf("Move disk %d from %s to %s. \n",n,src,dst);
+        TOH(n-1,aux,src,dst);
     }
 }
 
 int main(){
     int n;
+    char src[] = {'s','o','u','r','c','e','\0'};
+    char aux[] = {'a','u','x','i','l','i','a','r','y','\0'};
+    char dst[] = {'d','e','s','t','i','n','a','t','i','o','n','\0'};
+
     printf("Enter number of disks : ");
     scanf("%d",&n);
-    TOH(n,'A','B','C');
+    TOH(n,src,aux,dst);
+
     return 0;
 }
