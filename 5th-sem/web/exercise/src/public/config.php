5096ca3fd0ed8f57f96c2b07d9b7fc4ef52541a8
iambasanta <sangpangbasanta@gmail.com>
2022-12-18 20:07:50 +0545

    Create config file

---
+++ 5th-sem/web/exercise/src/public/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'database' => [
+        'host' => 'db',
+        'port' => 3306,
+        'dbname' => 'demo',
+        'charset' => 'utf8mb4'
+    ],
+];
--- 5th-sem/web/exercise/src/public/index.php
+++ 5th-sem/web/exercise/src/public/index.php
@@ -3,7 +3,9 @@
 require "functions.php";
 require "Database.php";
 
-$db = new Database();
+$config = require("config.php");
+
+$db = new Database($config["database"]);
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);;
 
 dd($posts);
