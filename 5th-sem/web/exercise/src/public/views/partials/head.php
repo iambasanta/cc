f5a9dec591037f6a658ac7866ee098a97c0dd85a
iambasanta <sangpangbasanta@gmail.com>
2022-12-18 18:09:39 +0545

    Build a site with basic routing

---
+++ 5th-sem/web/exercise/src/public/views/partials/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+    <style>
+    </style>
+  <body class="h-full">
+    <div class="min-h-full">
