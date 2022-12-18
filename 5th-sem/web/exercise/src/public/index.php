a987bc7ed1ddead9ca7ce7c9099f7f3b9f736a4c
iambasanta <sangpangbasanta@gmail.com>
2022-12-18 11:28:47 +0545

    Get. Set. Go!

--- 5th-sem/web/exercise/src/public/index.php
+++ 5th-sem/web/exercise/src/public/index.php
@@ -1,5 +1,33 @@
-<?php
-    echo"<pre>";
-    print_r($_SERVER);
-    echo"</pre>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Demo</title>
+  </head>
+    <style>
+        body{
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+  <body>
+    <?php 
+        $language = "PHP";
+        $learnt = false;
+
+        if($learnt){
+            $message = "Yay, I'm learning PHP.";
+        }else{
+            $message = "Whoss, I'm not learning PHP.";
+        }
+    ?>
+
+    <h1>
+    <?php echo $message; ?>!
+    </h1>
+
+  </body>
+</html>
