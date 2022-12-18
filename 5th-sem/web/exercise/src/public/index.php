be290aa6e1840e0286ab1ab25582435df4df7296
iambasanta <sangpangbasanta@gmail.com>
2022-12-18 18:12:40 +0545

    Use basic router for routing

--- 5th-sem/web/exercise/src/public/index.php
+++ 5th-sem/web/exercise/src/public/index.php
@@ -1,34 +1,5 @@
 <?php
 
-$books = [
-    [
-        "name" => "Refactoring UI",
-        "author" => "Adam Wathan",
-        "releaseYear" => "2008",
-        "purchaseUrl" => "https://www.refactoringui.com/",
-    ],
-    [
-        "name" => "Clean Code",
-        "author" => "Bob C. Martin",
-        "releaseYear" => "2020",
-        "purchaseUrl" => "https://www.amazon.com",
-    ],
-    [
-        "name" => "Eloquent javascript",
-        "author" => "Marjin Haverbeke",
-        "releaseYear" => "2018",
-        "purchaseUrl" => "https://www.amazon.com",
-    ],
-    [
-        "name" => "Clean Architecture",
-        "author" => "Bob C. Martin",
-        "releaseYear" => "2017",
-        "purchaseUrl" => "https://www.amazon.com",
-    ],
-];
+require "functions.php";
 
-$filteredBooks = array_filter($books, function ($book){
-    return $book["releaseYear"] >= 1950 && $book["releaseYear"] <=2020;
-});
-
-require "index.view.php";
+require "router.php";
