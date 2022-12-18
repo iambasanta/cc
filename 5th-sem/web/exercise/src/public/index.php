1b29713f267b26b9e719f2d3119aa8073b878452
iambasanta <sangpangbasanta@gmail.com>
2022-12-18 12:17:08 +0545

    Learn about functions

--- 5th-sem/web/exercise/src/public/index.php
+++ 5th-sem/web/exercise/src/public/index.php
@@ -13,29 +13,51 @@
         $books = [
             [
                 "name" => "Refactoring UI",
+                "author" => "Adam Wathan",
                 "releaseYear" => "2008",
                 "purchaseUrl" => "https://www.refactoringui.com/",
             ],
             [
                 "name" => "Clean Code",
+                "author" => "Bob C. Martin",
                 "releaseYear" => "2020",
                 "purchaseUrl" => "https://www.amazon.com",
             ],
             [
                 "name" => "Eloquent javascript",
+                "author" => "Marjin Haverbeke",
                 "releaseYear" => "2018",
                 "purchaseUrl" => "https://www.amazon.com",
             ],
+            [
+                "name" => "Clean Architecture",
+                "author" => "Bob C. Martin",
+                "releaseYear" => "2017",
+                "purchaseUrl" => "https://www.amazon.com",
+            ],
         ];
+
+        function filterByAuthor($books, $author){
+            $filteredBooks = [];
+
+            foreach ($books as $book){
+                if($book["author"] === $author){
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+
+        }
      ?>
 
     <ul>
-        <?php foreach($books as $book) : ?>
-        <li> 
-            <a href="<?= $book["purchaseUrl"] ?>">
-                <?= $book["name"]?> (<?=$book["releaseYear"] ?>)
-            </a>
-         </li>
+        <?php foreach(filterByAuthor($books, "Bob C. Martin") as $book) : ?>
+            <li> 
+                <a href="<?= $book["purchaseUrl"] ?>">
+                <?= $book["name"]?> (<?=$book["releaseYear"] ?>) - By <?= $book["author"] ?>
+                </a>
+             </li>
         <?php endforeach; ?>
     </ul>
 
