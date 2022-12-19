c4f69715985c953609dc2641470b502d1debd684
iambasanta <sangpangbasanta@gmail.com>
2022-12-19 09:04:02 +0545

    Only run secured queries

--- 5th-sem/web/exercise/src/public/index.php
+++ 5th-sem/web/exercise/src/public/index.php
@@ -6,6 +6,11 @@ require "Database.php";
 $config = require("config.php");
 
 $db = new Database($config["database"]);
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);;
+
+$id  = $_GET['id'];
+
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, ['id'  => $id])->fetch();;
 
 dd($posts);
