c4ee64b166dfeb35c676934d01cc07a0533f259d
iambasanta <sangpangbasanta@gmail.com>
2023-04-29 10:42:13 +0545

    Add program to demonstrate class properties

---
+++ 6th-sem/netcentric/ClassProperties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassProperties
+{
+    class Student
+    {
+        private string name;
+        private int rollNo;
+
+        // property
+        public string Name 
+        {
+            get { return name; }   // get method
+            set { name = value; }  // set method
+        }
+
+        public int RollNo
+        {
+            get { return rollNo; }   // get method
+            set { rollNo = value; }  // set method
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+
+            // calls set
+            s1.Name = "Hisagi";
+            s1.RollNo = 69;
+
+            // calls get
+            Console.WriteLine("NAME : " + s1.Name);
+            Console.WriteLine("ROLLNO : " + s1.RollNo);
+        }
+    }
+}
