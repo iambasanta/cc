6962772f32a5668be987a0702c1d99d2f5ccc1b1
iambasanta <sangpangbasanta@gmail.com>
2023-05-01 22:33:47 +0545

    Add program to demonstrate class properties

---
+++ 6th-sem/netcentric/MethodOverloading/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+
+    class Calculation
+    {
+        public void calc()
+        {
+           Console.WriteLine("hello");
+        }        
+
+        public void calc(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }        
+
+        public void calc(double x, double y)
+        {
+           Console.WriteLine(x * y);
+        }        
+
+        public void calc(int x, int y, int z)
+        {
+           Console.WriteLine((x * y) + z);
+        }        
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c1 = new Calculation();
+
+            // calls calc with no parameters
+            c1.calc();
+
+            // calls calc with two int parameters
+            c1.calc(33, 36);
+
+            // calls calc with two double parameters
+            c1.calc(3.3, 3.6);
+
+            // calls calc with three parameters
+            c1.calc(33, 2, 3);
+
+        }
+    }
+}
