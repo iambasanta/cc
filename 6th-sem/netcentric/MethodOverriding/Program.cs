0f7a28aa15e07bfa339499d84fe5726877580ed8
iambasanta <sangpangbasanta@gmail.com>
2023-05-02 07:26:15 +0545

    Add program to demonstrate method overriding

---
+++ 6th-sem/netcentric/MethodOverriding/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+           Console.WriteLine("Eating...");
+        }        
+    }
+
+    class Dog: Animal
+    {
+       public override void eat()
+       {
+           Console.WriteLine("Eating bone...");
+       } 
+    }
+
+    class Cat: Animal
+    {
+       public override void eat()
+       {
+           Console.WriteLine("Eating milk...");
+       } 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            a.eat();
+
+            Dog d = new Dog();
+            d.eat();
+
+            Cat c = new Cat();
+            c.eat();
+        }
+    }
+}
