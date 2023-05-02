c4e24040ea29efcd2e42593f510563355e647a22
iambasanta <sangpangbasanta@gmail.com>
2023-05-02 07:32:37 +0545

    Add program to demonstrate interface

---
+++ 6th-sem/netcentric/DemoInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DemoInterface
+{
+    public interface IDrawable
+    {
+        void draw();        
+    }
+
+    public class Rectangle : IDrawable
+    {
+       public void draw()
+       {
+           Console.WriteLine("Drawing rectangle...");
+       } 
+    }
+
+    public class Circle : IDrawable
+    {
+       public void draw()
+       {
+           Console.WriteLine("Drawing circle...");
+       } 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDrawable d;
+            d = new Rectangle();
+            d.draw();
+
+            d = new Circle();
+            d.draw();
+        }
+    }
+}
